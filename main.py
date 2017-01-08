1b35f6ac76fac30f2930a44c2a0037aa3c4f5c42
felix <felix.martel@polytechnique.edu>
2017-01-08 18:05:48 +0100

    La question 1.3 fonctionne

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import os
 import math
 from operator import itemgetter
 import heapq
-
+import webbrowser as web
 
 t0 = time()
 
@@ -33,37 +33,48 @@ demo = {
         'start': 2638124123         
     }
 }
-dataset = 'man'
-default_time = 5*60*1000
+dataset = 'malta'
+default_time = 5
 startingPoint = demo[dataset]['start'] # id du sommet de départ
+G = {}
+coordinates = {}
 
 ### TRAITEMENT DU JEU DE DONNEES ###
 # <G> liste d'adjacence qui représente le graphe fourni
 # <coordinates> sert uniquement à la visualisation : associe à chaque id ses
 # coordonnées
-print('Traitement des données... \n')
-G = {}
-coordinates = {}
-file = open(demo[dataset]['set'], 'r', newline='\n')
-#~ compteur = 1
-#~ nombrelignes = sum(1 for _ in file)
-for line in file:
-    data = line.split(" ")
-    if data[0] == 'v':
-        G[int(data[1])] = {}
-        coordinates[int(data[1])] = [int(data[3])/1000000, int(data[2])/1000000]
-    elif data[0] == 'a':
-        G[int(data[1])][int(data[2])] = int(data[3][:-1])
-    #~ print("Pourcentage d'accomplissement :", compteur/nombrelignes*100)
-    #~ compteur += 1
-file.close()
-print("Données traitées en", time()-t0, "secondes")
+
+def processData(location=dataset):
+    global dataset
+    global startingPoint
+    t0 = time()
+    dataset = location
+    startingPoint = demo[dataset]['start']
+    print('Traitement des données... \n')
+    G.clear()
+    coordinates.clear()
+    file = open(demo[dataset]['set'], 'r', newline='\n')
+    #~ compteur = 1
+    #~ nombrelignes = sum(1 for _ in file)
+    for line in file:
+        data = line.split(" ")
+        if data[0] == 'v':
+            G[int(data[1])] = {}
+            coordinates[int(data[1])] = [int(data[3])/1000000, int(data[2])/1000000]
+        elif data[0] == 'a':
+            G[int(data[1])][int(data[2])] = int(data[3][:-1])
+        #~ print("Pourcentage d'accomplissement :", compteur/nombrelignes*100)
+        #~ compteur += 1
+    file.close()
+    print("Données traitées en", time()-t0, "secondes")
+
 
 def getShortestDistances(quandsarreter):
 	# ni plus ni moins qu'un algo de Djikstra
 	# retourne le dictionnaire contenant pour clés les sommets de G et pour valeurs leur (plus courte) distance à SP
 	# l'algorithme s'arrête quand la plus courte distance de startingPoint au sommet courant est plus grande que quandsarreter
 	# en effet, cette distance augmente toujours au cours de l'éxecution de l'algorithme
+	quandsarreter = getTime(quandsarreter)
 	pluscourtedistance = dict([(s, math.inf if s!=startingPoint else 0) for s in G])
 	sommetsavisiter = heapq.heapify([s for s in G], key = lambda s : pluscourtedistance[s])
 
@@ -82,13 +93,14 @@ def getShortestDistances(quandsarreter):
 def getIsochronePab(D):
 	data = {startingPoint: 0}
 	result = []
+	D = getTime(D)
      
 	while data:
 		sommet, distanceSommet = min(data.items(), key = itemgetter(1))
 		
 		
 		
-		print("{:10.2f}".format(distanceSommet / D * 100)) # indicateur d'avancement
+		#print("{:10.2f}".format(distanceSommet / D * 100)) # indicateur d'avancement
 		
 		if distanceSommet >= D: 
 			result.append(sommet)
@@ -101,49 +113,31 @@ def getIsochronePab(D):
 						data[voisin] = distanceSommet + dvoisin
 		del data[sommet] # ne pas conserver 
 		del G[sommet] # pour ne pas revenir en arrière
+	print(" ---------------------------- ISOCHRONE ", D , " milliseconds ")
 	print(result)
 	return result
-			
 
-def getIsochronePab(D):
-	data = {startingPoint: 0}
-	result = []
-	
-	while data:
-		sommet, distanceSommet = min(data.items(), key = itemgetter(1))
-		
-		
-		
-		print("{:10.2f}".format(distanceSommet / D * 100)) # indicateur d'avancement
-		
-		if distanceSommet >= D:
-			result.append(sommet)
-		else:
-			for voisin, dvoisin in G[sommet].items():
-				if voisin in G:
-					if voisin in data :
-						data[voisin] = min(distanceSommet + dvoisin, data[voisin])
-					else:
-						data[voisin] = distanceSommet + dvoisin
-		del data[sommet] # ne pas conserver 
-		del G[sommet] # pour ne pas revenir en arrière
-	print(result)
-	return result   
-
+def getTime(t):
+    return t * 60 *1000
+    
 def getClosestElement(data):
     return min(data.items(), key = itemgetter(1))
+ 
+distance = {}
+previousVertex = {}
     
-def getPseudoisochrone(d1, d2):
-      d1 = 60000 * d1
-      d2 = 60000 * d2
+def getPseudoisochrone(d1=default_time, d2=2*default_time, debug=False):
+      d1 = getTime(d1)
+      d2 = getTime(d2)
       # Constantes
       SELECTED = False
       VISITED = True
       
       # Initialisation
-      distance = {}
-      previousVertex = {}
+      
       result = []
+      
+      test = []
       
       data = {startingPoint: 0}
       visitedVertices = {startingPoint: SELECTED}
@@ -151,32 +145,46 @@ def getPseudoisochrone(d1, d2):
       
       while currentDistance < d2:
           for currentNeighbor, distanceNeighbor in G[currentVertex].items():
-              if (currentNeighbor not in visitedVertices) or (not visitedVertices[currentNeighbor]):
-                  if (currentNeighbor not in visitedVertices) or ((visitedVertices[currentNeighbor] == SELECTED) and (currentDistance + distanceNeighbor < data[currentNeighbor])):
-                      data[currentNeighbor] = currentDistance + distanceNeighbor
-                      distance[currentNeighbor] = currentDistance + distanceNeighbor
-                      previousVertex[currentNeighbor] = currentVertex
+              if (currentNeighbor not in visitedVertices) or ((visitedVertices[currentNeighbor] == SELECTED) and (currentDistance + distanceNeighbor < data[currentNeighbor])):
+                  data[currentNeighbor] = currentDistance + distanceNeighbor
+                  visitedVertices[currentNeighbor] = SELECTED
+                  distance[currentNeighbor] = currentDistance + distanceNeighbor
+                  previousVertex[currentNeighbor] = currentVertex
+#                  if (currentNeighbor not in visitedVertices) or ((visitedVertices[currentNeighbor] == SELECTED) and (currentDistance + distanceNeighbor < data[currentNeighbor])):
+#                      data[currentNeighbor] = currentDistance + distanceNeighbor
+#                      distance[currentNeighbor] = currentDistance + distanceNeighbor
+#                      previousVertex[currentNeighbor] = currentVertex
           #fin pour tout
           
-          if currentDistance < d2:
-              del data[currentVertex]
-              visitedVertices[currentVertex] = VISITED
+          del data[currentVertex]
+          visitedVertices[currentVertex] = VISITED
+          test.append(currentVertex)
           currentVertex, currentDistance = getClosestElement(data)
-      
-      DEV_data = data.copy()
+      #print(" --------------- PSEUDO ISOCHRONES ", d2 ," milliseconds -----")
+      #print(data)
       # Fin de l'étape 1
       
-      for vertex, reverseDistance in data.items():
-          currentDistance = 0
-          while currentDistance < reverseDistance - d1:
+      paths = []
+      for vertex, distanceFromStart in data.items():
+          #print("Distance from start : ", distanceFromStart/60000)
+          path = []
+          while distanceFromStart > d1:
               vertex = previousVertex[vertex]
-              currentDistance += distance[vertex]
+              distanceFromStart = distance[vertex]
+              path.append(vertex)
           result.append(vertex)
-      return result, DEV_data
+          if debug:
+              paths.append(path)
+          #print("Distance parcourue : ", currentDistance/60000)
+      if debug:
+          return paths
+      return [result, data]
+
 			
 	
 def vizIsochroneDjikstra(exact_time = default_time):
 	delta_time = 1000 # intervalle de temps autorisé quand on dit "exactement", ici 1 seconde
+	exact_time = getTime(exact_time) 
 	I = getShortestDistances(quandsarreter = exact_time + delta_time + 1)
 	visualize([ s for s in I if I[s] > exact_time - delta_time and I[s] < exact_time + delta_time])
     
@@ -203,6 +211,10 @@ def visualize(I):
 	os.system('firefox vis/vis.html')
 
 def visualizeMany(L):
+    """
+        Each element L[i] of L is a list of vertices 
+        Make sure your vis/vis.html file is up-to-date
+    """
     I = L[0]
     file = open('vis/points.js', 'w')
     file.write('var plottedPoints = [\n')
@@ -215,7 +227,7 @@ def visualizeMany(L):
     file.write('\n;\n')
     
     file.write('var pointList = [\n')
-    for I in L[1:]:
+    for I in L:
         file.write('[\n')
         for vertex in I:
             file.write(str(coordinates[vertex]))
@@ -224,14 +236,45 @@ def visualizeMany(L):
     file.write('\n];')    
     file.close()
     # os.system('firefox vis/vis.html')
-    
-def run(D=default_time, output="viz"):
-    I = getIsochrone(D)
-    if output == "viz":
-        visualize(I)
-    elif output == "list":
-        exportPointList(I)
-    else:
-        return(I)
 
+def viz(L):
+    try: # L est une liste de liste
+        origin = L[0][0]
+        visualizeMany(L)
+    except TypeError: # L est une liste
+        visualizeMany([L])
+
+
+def compareIsochroneAndPseudoisochrone():
+    processData()
+    t1, t2 = 5, 10
+    I, J = getPseudoisochrone(t1, t2)
+    processData()
+    K = getIsochronePab(t1)
+    processData()
+    L = getIsochronePab(t2)
+    isochrones = [I, J, K, L]
+    visualizeMany(isochrones)
+    see()
+    return isochrones
+
+def test(d1=default_time, d2=2*default_time, loc=dataset, debug=False):
+    print(" --- Testing function : getPseudoisochrone  with parameters : ")
+    print("Dataset : ", loc)
+    print("Destination time : ", d2)
+    print("Halfway time : ", d1)
+    if loc != dataset:
+        processData(loc)
+    l = getPseudoisochrone(d1, d2, debug)
+    viz(l)
+    see()
+
+def see():
+    if os.name == 'nt':
+        path = 'file:///' + os.getcwd().replace('\\', '/') + '/vis/vis.html'
+        web.open(path) #Pour Chrome/Windows
+    else:
+        os.system('firefox vis/vis.html') #Pour Firefox/Linux
+
+processData()
 #visualize(getIsochronePab2(default_time))
